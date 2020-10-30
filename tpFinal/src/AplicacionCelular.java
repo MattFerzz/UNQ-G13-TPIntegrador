4398eb51f67e6b13edfbb60233a7f499aafe2911
Lautaro <lautaro.ml@gmail.com>
2020-10-30 16:08:49 -0300

    agregado el server

--- tpFinal/src/AplicacionCelular.java
+++ tpFinal/src/AplicacionCelular.java
@@ -14,12 +14,12 @@ public class AplicacionCelular {
 	private float saldo = 0;
 	private LocalTime estacionamientoActivo;
 	
-	public string iniciarEstacionamiento(string patente) {
-		string ret;
+	public String iniciarEstacionamiento(String patente) {
+		String ret;
 		if (saldo >= 40) {
-			float horasEnSaldo = Math.floor(saldo/40);
-			LocalTime horaMaxima = LocalTime.now().addHours(horasEnSaldo);
-			ret = "Hora actual: " + str(LocalTime.now()) + ", Hora máxima: " + horaMaxima;
+			double horasEnSaldo = Math.floor(saldo/40);
+			LocalTime horaMaxima = LocalTime.now().plusHours((long) horasEnSaldo);
+			ret = "Hora actual: " + LocalTime.now() + ", Hora máxima: " + horaMaxima;
 			estacionamientoActivo = LocalTime.now();
 		} else {
 			ret = "Saldo insuficiente. Estacionamiento no permitido.";
@@ -27,11 +27,11 @@ public class AplicacionCelular {
 		return ret;
 	}
 	
-	public string finalizarEstacionamiento() {
+	public String finalizarEstacionamiento() {
 		float duracion = ((estacionamientoActivo.getMinute() > LocalTime.now().getMinute())) ?
-						 LocalTime.now().getHour() - estacionamientoActivo.getHour :
-						 LocalTime.now().getHour() - estacionamientoActivo.getHour - 1;
-		return "Horario de inicio: " + str(estacionamientoActivo) + ", horario de finalizacion: " + str(LocalTime.now()) + ", duracion: " + duracion + " horas, costo: " + duracion*40;
+						 LocalTime.now().getHour() - estacionamientoActivo.getHour() :
+						 LocalTime.now().getHour() - estacionamientoActivo.getHour() - 1;
+		return "Horario de inicio: " + estacionamientoActivo + ", horario de finalizacion: " + LocalTime.now() + ", duracion: " + duracion + " horas, costo: " + duracion*40;
 	}
 	
 	public void cargarSaldo(float x) {
--- tpFinal/src/appTest.java
+++ tpFinal/src/appTest.java
@@ -1,4 +1,5 @@
-import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
 
 /*
@@ -16,40 +17,44 @@ public class appTest {
 	AplicacionCelular app2 = new AplicacionCelular();
 	AplicacionCelular app3 = new AplicacionCelular();
 	
-	@setUp
-	public void setUp() {
+	@Before
+	protected void setUp() {
 		app1.cargarSaldo(130);
 		app2.cargarSaldo(1000);
 		app3.cargarSaldo(10);
 	};
 	
-	@test
-	public void iniciarEstacionamiento(string p) {
-		string resultado1 = app1.iniciarEstacionamiento();
-		string resultado2 = app2.iniciarEstacionamiento();
-		string resultado3 = app3.iniciarEstacionamiento();
+	@Test
+	public void iniciarEstacionamiento(String p) {
+		String resultado1 = app1.iniciarEstacionamiento("abc123");
+		String resultado2 = app2.iniciarEstacionamiento("bcd234");
+		String resultado3 = app3.iniciarEstacionamiento("cde345");
 		assertEquals(resultado1, "Horario de inicio: 16:15, horario máximo: 19:15");
 		assertEquals(resultado2, "Horario de inicio: 16:15, horario máximo: 20:00");
 		assertEquals(resultado3, "Saldo insuficiente. Estacionamiento no permitido.");
 	}
 	
+	@Test
 	public void finalizarEstacionamiento() {
-		string resultado1 = app1.finalizarEstacionamiento();
+		String resultado1 = app1.finalizarEstacionamiento();
 		assertEquals(resultado1, "Horario de inicio: 16:15, horario de finalizacion: 18:45, duracion: 2 horas, costo: 80");
 	};
 
+	@Test
 	public void cargarSaldo() {
-		float saldoPrevio = app.consultarSaldo();
+		float saldoPrevio = app3.consultarSaldo();
 		app3.cargarSaldo(100);
-		assertEquals(app3.consultarSaldo(), saldoPrevio + 100);
+		assertEquals(app3.consultarSaldo(), saldoPrevio + 100, 0);
 	};
 	
+	@Test
 	public void consultarSaldo() {
-		assertEquals(app1.consultarSaldo, 130);
-		assertEquals(app2.consultarSaldo, 1000);
-		assertEquals(app3.consultarSaldo, 10);
+		assertEquals(app1.consultarSaldo(), 130, 0);
+		assertEquals(app2.consultarSaldo(), 1000, 0);
+		assertEquals(app3.consultarSaldo(), 10, 0);
 	}
 	
+	@Test
 	public void setSate() {
 		//no se si cubrir setState con un test
 	};
